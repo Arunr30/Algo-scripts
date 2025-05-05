515f8fb87ff9acf9060d940c6e74f33d5e9492f4
Arunvasuacchu <arunvasur@gmail.com>
2025-05-05 14:49:38 +0530

    fixed browserClient

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import {
@@ -5,11 +7,12 @@ import {
   LoginLink,
   LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export async function Navbar() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+
+export function Navbar() {
+  const { getUser } = useKindeBrowserClient();
+  const user = getUser();
 
   return (
     <nav className="bg-white py-4 px-8 flex items-center justify-between shadow-md">
--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,11 @@ const nextConfig: NextConfig = {
         hostname: 'lh3.googleusercontent.com',
         protocol: "https",
       }
+      ,
+      {
+        hostname: "i.ytimg.com",
+        protocol: "https"
+      }
     ]
   }
 };
